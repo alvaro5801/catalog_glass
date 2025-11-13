951f63da12cf2a0fa44db1f0c022341a2f3f135a
Alvaro Silva <alvaro.rs580@gmail.com>
2025-11-13 15:33:39 -0300

    upload 2.1.2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.vercel-storage.com', // Permite todas as imagens do Vercel Blob
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // Útil para placeholders se usar
+      }
+    ],
+  },
 };
 
 export default nextConfig;
